8c2262d5429744f20d4d4576a97ce3ac20cba2de
Joseph <fallyfox@gmail.com>
2023-01-31 14:22:02 +0100

    safeareaview setup

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View,Text } from "react-native";
+import { CustomSafeAreaView } from "./components/CustomSafeAreaView";
 
-export default function App() {
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <CustomSafeAreaView>
+      <Text>Kasuwa parent screen</Text>
+    </CustomSafeAreaView>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/CustomSafeAreaView.js
@@ -0,0 +1,16 @@
+import { View,StyleSheet } from "react-native";
+
+export function CustomSafeAreaView ({children}) {
+    return (
+        <View style={styles.container}>
+            {children}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'magenta'
+    }
+})
