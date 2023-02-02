74d39a7b08ca27c591697a83469aeddfed61d597
Joseph Ogbu <fallyfox@gmail.com>
2023-02-02 17:58:33 +0100

    part of home screen ui

---
+++ screens/Home.js
@@ -0,0 +1,60 @@
+import { View,Text,StyleSheet,ScrollView,Image,TouchableOpacity } from "react-native";
+import { CustomSafeAreaView } from "../components/CustomSafeAreaView";
+
+export function Home () {
+    return (
+        <CustomSafeAreaView>
+            <ScrollView>
+            <View style={styles.content}>
+                <Text style={styles.brandName}>Kasuwa</Text>
+                <Image 
+                style={styles.carouselImage} 
+                source={require('../assets/images/flash.jpg')}/>
+            </View>
+
+            <View style={styles.deals}>
+                <Text style={styles.dealsHeading}>Recent deals</Text>
+                <TouchableOpacity style={styles.moreDeals}>
+                    <Text>See all deals</Text>
+                </TouchableOpacity>
+            </View>
+            </ScrollView>
+        </CustomSafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    content:{
+        paddingHorizontal:10
+    },
+    brandName:{
+        fontSize:30,
+        fontWeight:'bold',
+        color:'purple',
+        marginBottom:20
+    },
+    carouselImage:{
+        height:100,
+        width:'100%',
+        borderRadius:8
+    },
+    deals:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        padding:12,
+        borderTopLeftRadius:8,
+        borderTopRightRadius:8,
+        backgroundColor:'#E98EAD',
+        marginTop:20,
+        marginHorizontal:10
+    },
+    dealsHeading:{
+        fontSize:22
+    },
+    moreDeals:{
+        color:'#FFC6D3'
+    }
+})
+
+// FEA1BF
