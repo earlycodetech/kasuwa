dc27d2e01aff13202ba51f9404a3380073290428
HASHIM ABDULRAZAQ <ayomide162002@gmail.com>
2023-02-06 18:06:53 +0100

    product template

--- screens/Home.js
+++ screens/Home.js
@@ -1,54 +1,117 @@
-import { View, Text, StyleSheet, ScrollView, Image , TouchableOpacity} from "react-native";
+import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from "react-native";
 import { CustomSafeAreaView } from "../components/CustomSafeAreaView";
+import { Themes } from "../assets/themes";
 export function Home() {
     return (
         <CustomSafeAreaView>
-         <ScrollView>
-         <View style={styles.content}>
-                <Text style={styles.brandName}>Kasuwa</Text>
-                <Image style={styles.corouselImage}
+
+            <View style={styles.content}>
+
+                <Image style={styles.carouselImage}
                     source={require('../assets/images/flash.jpg')} />
             </View>
             <View style={styles.deals}>
-                <Text style={styles.dealsHeading}>Recent Deals</Text>
-                <TouchableOpacity style={styles.moreDeals}>
-                    <Text>See all Deals</Text>
-                </TouchableOpacity>
+                <View style={[styles.recentDeals, { backgroundColor: Themes.colors.blue100 }]}>
+                    <Text style={styles.dealsHeading}>Recent Deals</Text>
+                    <TouchableOpacity style={styles.moreDeals}>
+                        <Text>See all Deals</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.productsList}>
+                    <TouchableOpacity style={styles.product}>
+                        <View style={{ alignItems: 'center', marginBottom: Themes.sizes[1] }}>
+                            <Image source={require('../assets/images/myfi.webp')} style={styles.thumbnail} />
+                        </View>
+                        <Text style={styles.productName}>Airtel Broadband 4G Rechargeable myfi</Text>
+                        <Text style={styles.productPrice}>₦15500</Text>
+                    </TouchableOpacity>
+                </View>
+
             </View>
-         </ScrollView>
+            <View style={styles.sponsored}>
+                <View style={[styles.recentDeals, { backgroundColor: Themes.colors.purple100 }]}>
+                    <Text style={styles.dealsHeading}>Recent Deals</Text>
+                    <TouchableOpacity style={styles.moreDeals}>
+                        <Text>See all Deals</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.productsList}>
+                    <TouchableOpacity style={styles.product}>
+                        <View style={{ alignItems: 'center', marginBottom: Themes.sizes[1] }}>
+                            <Image source={require('../assets/images/myfi.webp')} style={styles.thumbnail} />
+                        </View>
+                        <Text style={styles.productName}>Airtel Broadband 4G Rechargeable myfi</Text>
+                        <Text style={styles.productPrice}>₦15500</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
         </CustomSafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    content:{
-        paddingHorizontal:10
+    content: {
+        flex: 1,
+        paddingHorizontal: Themes.sizes[3],
+        justifyContent: 'center'
     },
     brandName: {
-        fontSize: 20,
-        fontWeight:'bold',
-        color: 'purple',
-        marginBottom:20
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: Themes.colors.purple500,
+        marginBottom: 20
     },
-    carouselImage:{
-        height:100,
-        width:'100%',
-        borderRadius:8
+    carouselImage: {
+        height: 100,
+        width: '100%',
+        borderRadius: 8
     },
-    deals:{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        alignItems:'center',
-        padding:12,
-        backgroundColor:'E98EAD',
-        borderTopRadius:8,
-        marginTop:20,
-        marginHorizontal:10,
+    deals: {
+        marginVertical: Themes.sizes[2],
+        marginHorizontal: Themes.sizes[2],
+        flex: 2.5
     },
-    dealsHeading:{
-        fontSize:22
+    recentDeals: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 12,
+        borderTopLeftRadius: 8,
+        borderTopRightRadius: 8
     },
-    moreDeals:{
-        color:'#FFC6D3'
+    dealsHeading: {
+        fontSize: 22
+    },
+    moreDeals: {
+        color: '#FFC6D3'
+    },
+    sponsored: {
+        flex: 2.5,
+        paddingHorizontal: Themes.sizes[2]
+    },
+    productsList: {
+        flex:2.5,
+        marginTop: Themes.sizes[2]
+    },
+    product: {
+        flex: 1,
+        width: 200,
+        padding: Themes.sizes[2],
+        borderWidth: 1,
+        bordercolor: Themes.colors.gray400,
+        borderRadius: 10
+    },
+    thumbnail: {
+        height: 120,
+        width: 120
+    },
+    productName: {
+        fontSize: Themes.fontSizePoint.title
+    },
+    productPrice: {
+        fontSize: Themes.fontSizePoint.title,
+        fontWeight: 'bold',
+        marginTop: Themes.sizes[1]
     }
 })
