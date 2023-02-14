04fa95c5f1112d04b79a3571fafd203e0b82700f
Bello Shuaibu <naijacelebrity@gmail.com>
2023-02-14 15:07:29 +0100

    corrections

--- assets/Carousel-messages.js
+++ assets/Carousel-messages.js
@@ -1,18 +1,15 @@
 export const carouselMessages = [
     {
         id:'ggetet',
-        img:require('./assets/images/carousel/flash.jpg')
+        img:require('../assets/images/carousel/flash.jpg')
     },
     {
         id:'yeye9',
-        img:require('./assets/images/carousel/intel.jpg')
+        img:require('../assets/images/carousel/Intel.jpg')
     },
     {
         id:'gg778et',
-        img:require('./assets/images/carousel/payday.jpg')
+        img:require('../assets/images/carousel/payday.jpg')
     },
-    {
-        id:'ppytr',
-        img:require('./assets/images/carousel/samsung.webp')
-    }
+   
 ]
