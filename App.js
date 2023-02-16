7d7eab40967fe538c2fe2520024b561e2d90a468
HASHIM ABDULRAZAQ <ayomide162002@gmail.com>
2023-02-16 17:47:54 +0100

    app provider

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native"
 import { NativeStackNavigator } from "./infrastructure/NativeStackNavigator"
+import { AppProvider } from "./components/AppContext"
 
-export default function App () {
-  return(
-    <NavigationContainer>
-      <NativeStackNavigator/>
-    </NavigationContainer>
+export default function App() {
+  return (
+    <AppProvider>
+      <NavigationContainer>
+        <NativeStackNavigator />
+      </NavigationContainer>
+    </AppProvider>
   )
 }
